7c067898a0ed52706927210e277090ce58713063
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-11 23:49:56 -0600

    Add: header.php
    	header component, uses a table that fetch routes info

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<?php include('includes/database.php') ?>
+
+<header class="header">
+  <section class="brand-container">
+    <img src="https://via.placeholder.com/250x40" alt="example brand icon">
+    <h1>Taskion</h1>
+  </section>
+  <ul class="menu">
+    <?php 
+      $result = executeQuery(true, "SELECT * FROM routes");
+      foreach($result as $row) {
+        $route_name = $row['route_name'];
+        $route_path = $row['route_path'];
+
+        echo 
+        "<li class='item'>
+          <a class='item-link' href='$route_path'>$route_name</a>
+        </li>";
+      }
+    ?>
+  </ul>
+</header>
