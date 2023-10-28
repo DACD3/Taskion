7cf1bbe3ee1e324b8fffb53630b4ffcc3e38707e
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-28 12:57:51 -0600

    Update: index.php
    	Include header and index css files and change lang to es

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Taskion - a trello clone</title>
   <?php include('includes/styles.php') ?>
   <?php include_once('includes/config.php') ?>
+  <link rel="stylesheet" href="./assets/css/header.css">
+  <link rel="stylesheet" href="./assets/css/index.css">
 </head>
 <body>
   <?php include('includes/header.php') ?>
