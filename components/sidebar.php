b7c58ee802f6b98e99eeb8391703b12fee721ff3
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-29 21:03:00 -0600

    Add: sidebar component
    	This component is used in the route app

---
+++ components/sidebar.php
@@ -0,0 +1,42 @@
+<?php
+  $user = $_SESSION['user'];
+
+  $image = base64_encode($user->getAvatar());
+  
+  $image_alt = substr($user->getName(), 0, 7);
+
+  $avatarEl = "<img class='avatar' title='' width='64' height='64' src='data:image/png;base64,$image' alt='$image_alt avatar' />";
+?>
+
+<header class="sidebar" id="sidebar">
+
+  <section class="sidebar-toggler">
+
+    <button class="button" id="toggler" title="sidebar toggler">
+      <span class="icon fas fa-bars fa-2xl"></span>
+    </button>
+
+  </section>
+
+  <ul class="menu">
+
+    <li class="menu-item">
+      <span class="icon fas fa-list-check fa-xl"></span>
+      <a href="" class="menu-link">Tareas</a>
+    </li>
+
+    <li class="menu-item">
+      <span class="icon fas fa-folder-closed fa-xl"></span>
+      <a href="" class="menu-link">Proyectos</a>
+    </li>
+
+  </ul>
+
+  <section class="user">
+    <?php echo $avatarEl ?>
+    <p>
+      <?php echo $image_alt ?>
+    </p>
+  </section>
+
+</header>
