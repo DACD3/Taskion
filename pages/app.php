39df3b04d063f90e18c0c1845f1b85feba519f81
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-29 18:50:46 -0600

    Update: pages/app.php
    	Implement authentication system, and basic html structure

--- pages/app.php
+++ pages/app.php
@@ -1,1 +1,26 @@
-<h1>Meter componentes aca :p</h1>
+<?php
+
+  include_once('../models/User.php');
+  session_start();
+
+  if (empty($_SESSION)) {
+    header('Location: /login', true);
+    exit;
+  }  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=s, initial-scale=1.0">
+  <title>Taskion - App</title>
+  <?php include('../includes/styles.php') ?>
+  <link rel="stylesheet" href="./assets/css/app.css">
+</head>
+<body>
+  <?php 
+    $user = $_SESSION['user'];
+  ?>
+</body>
+</html>
