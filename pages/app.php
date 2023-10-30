c0d0a009a1903edc5955a76f3d2df253a085b7c3
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-29 21:05:43 -0600

    Update: pages/app.php
    	* Include user model first before deserialization
    
    	* Implement authtentication
    
    	* Add styles and scripts
    
    	* Add sidebar component

--- pages/app.php
+++ pages/app.php
@@ -1,26 +1,31 @@
 <?php
 
-  include_once('../models/User.php');
-  session_start();
+include_once('../models/User.php');
+session_start();
 
-  if (empty($_SESSION)) {
-    header('Location: /login', true);
-    exit;
-  }  
+if (empty($_SESSION)) {
+  header('Location: /login', true);
+  exit;
+}
+
+$user = $_SESSION['user'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=s, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Taskion - App</title>
   <?php include('../includes/styles.php') ?>
   <link rel="stylesheet" href="./assets/css/app.css">
+  <script defer src="./assets/js/initial.js"></script>
+  <script defer src="./assets/js/app.js"></script>
 </head>
 <body>
-  <?php 
-    $user = $_SESSION['user'];
-  ?>
+  <?php include('../components/sidebar.php') ?>
+  <main class="app content">
+    <h1>¡Bienvenido <?php echo substr($user->getName(), 0, 7) ?>!</h1>
+  </main>
 </body>
 </html>
