c0bd6a9bbd105932bde58619c2d518c1acbbfec2
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-30 01:25:42 -0600

    Add: models Project, Task
    	generate models Project and Task that are going to be used in
    	the application.

---
+++ models/Project.php
@@ -0,0 +1,30 @@
+<?php
+
+class Project {
+    private $id;
+    private $name;
+
+    // Constructor
+    public function __construct($id, $name) {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+}
---
+++ models/Task.php
@@ -0,0 +1,48 @@
+<?php
+
+class Task
+{
+  private $id;
+  private $name;
+  private $description;
+
+  // Constructor
+  public function __construct($id, $name, $description)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->description = $description;
+  }
+
+  // Getters
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function getDescription()
+  {
+    return $this->description;
+  }
+
+  // Setters
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+}
