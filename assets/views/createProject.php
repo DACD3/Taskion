1a8ed25423eb79db0e6b8b5dfa22668499c7db32
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-30 01:26:24 -0600

    Add: views/
    	* createProject view -> only contains form
    	and fields that are required to make the projects and tasks.
    	* createTask

---
+++ assets/views/createProject.php
@@ -0,0 +1,11 @@
+<form method="POST" action="/app?operation=createProject" class="">
+  <section class="form-group">
+    <label for="" class="label">Nombre del proyecto</label>
+    <input type="text" name="Name" id="Name">
+  </section>
+
+  <section class="form-group">
+    <button type="submit">Crear proyecto</button>
+  </section>
+
+</form>
---
+++ assets/views/createTask.php
@@ -0,0 +1,16 @@
+<form method="POST" action="/app?operation=createTask" class="">
+  <section class="form-group">
+    <label for="" class="label">Nombre de la tarea</label>
+    <input type="text" name="Name" id="Name">
+  </section>
+
+  <section class="form-group">
+    <label for="" class="label">Descripción</label>
+    <input type="text" name="Description" id="Description">
+  </section>
+
+  <section class="form-group">
+    <button type="submit">Crear proyecto</button>
+  </section>
+
+</form>
