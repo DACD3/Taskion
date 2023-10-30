eb487173f6d1b932b5b4d9e90d6d6122ee311cf0
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-29 23:19:15 -0600

    Update: includes/database.php
    	Migrate to PDO from mysqli

--- includes/database.php
+++ includes/database.php
@@ -1,48 +1,40 @@
 <?php
+
   $servername = "localhost"; // Servidor de la base de datos
   $username = "root"; // Nombre de usuario de la base de datos
   $password = getenv("DB_PASSWORD") !== false ? getenv("DB_PASSWORD") : ''; // Contraseña de la base de datos
   $database = "taskion"; // Nombre de la base de datos
+  
+  $dsn = "mysql:host=$servername;dbname=$database;charset=utf8mb4";
 
-  // Crear una nueva conexión a la base de datos e incluir la base de datos "taskion" en la conexión
-  $conn = new mysqli($servername, $username, $password, $database);
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+  ];
 
-  // Verificar la conexión y manejar errores en caso de falla
-  if ($conn->connect_error) {
-    die("Connection to database failed: " . $conn->connect_error);
-  }
+  $conn = new PDO($dsn, $username, $password, $options);
 
-  // Función para ejecutar consultas SQL
-// Función para ejecutar consultas SQL con parámetros
-  function executeQuery($IsSelectOperation, $sql, $values = array())
-  {
+
+  function executeQuery($isSelect, $query, $values = array()) {
+    
     global $conn;
 
-    // Preparar la consulta SQL
-    $stmt = $conn->prepare($sql);
+    try {
+      $stmt = $conn->prepare($query);
 
-    // Verificar si la preparación de la consulta fue exitosa
-    if ($stmt) {
-      // Unir los valores a la consulta preparada
-      if (!empty($values)) {
-        $types = str_repeat('s', count($values)); // Suponemos que todos los valores son cadenas (strings)
-        $stmt->bind_param($types, ...$values);
+      $stmt->execute($values);
+
+      if ($isSelect) {
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
       }
 
-      // Ejecutar la consulta preparada
-      $stmt->execute();
-
-      // Si la consulta es SELECT y la ejecución tiene éxito, devolver los resultados en un arreglo asociativo
-      if ($IsSelectOperation) {
-        $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC);
-      }
-
-      // Si la consulta es de otro tipo (INSERT, UPDATE, DELETE) y la ejecución tiene éxito, devolver true
-      return $stmt->affected_rows > 0;
-    } else {
-      // Si hubo un error en la preparación de la consulta, puedes manejarlo aquí
+      return $stmt->rowCount();
+    }
+    catch (PDOException $e) {
+      echo "Error: ". $e->getMessage() ."";
       return false;
     }
   }
+
 ?>
