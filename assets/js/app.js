0e68e38db0f8f54952a59699ea6895aa2bd08a46
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-10-30 01:23:43 -0600

    Update: assets/js/app.js
    	* Add functionality to sidebar toggler
    
    	* Add functionality to create buttons and add the request the
    	  selected view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,36 @@
-const sidebarEl = $('sidebar');
-const togglerEl = $('toggler');
+const VIEWS_URL = [
+  {
+    id: "create-project",
+    url: `${window.location.origin}/assets/views/createProject.php`, // Usa origin para evitar el problema del href
+  },
+  {
+    id: "create-task",
+    url: `${window.location.origin}/assets/views/createTask.php`, // Añade la URL correcta
+  },
+]
 
-togglerEl.addEventListener('click', () => {
+const sidebarEl = $("sidebar")
+const togglerEl = $("toggler")
 
-  sidebarEl.classList.toggle('open');
+togglerEl.addEventListener("click", () => {
+  sidebarEl.classList.toggle("open")
+})
 
+const handleClick = async (e) => {
+
+  const id = e.currentTarget.id;
+
+  const view = VIEWS_URL.find( v => v.id === id);
+
+  const res = await fetch(view.url)
+
+  const content = await res.text()
+
+  $("form-container").innerHTML = content
+}
+
+const actions = document.querySelectorAll(".button-link")
+
+actions.forEach(async (el) => {
+  el.addEventListener("click", handleClick)
 })
