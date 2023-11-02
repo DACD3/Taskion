b1c22bf99ecfd657faab40124990d3208803f85a
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-02 00:53:00 -0600

    Update: includes/queries.php
    	Use require_once import syntax instead of include or require and
    
    	use a different implementation when retrieving data from the
    	database.

--- includes/queries.php
+++ includes/queries.php
@@ -1,10 +1,12 @@
 <?php
 
-  include 'config.php';
+  require_once 'config.php';
+  require_once 'database.php';
 
   require_once('../models/User.php');
   require_once('../models/Project.php');
   require_once('../models/Task.php');
+
 
   function getUserData() {
     $user = $_SESSION['user'];
@@ -13,13 +15,15 @@
   }
 
   function getProjectsByUser() {
-
     $user = getUserData();
 
     $sql = "SELECT p.* FROM projects p JOIN users_has_projects up ON p.id = up.project_id WHERE up.user_id = ?";
 
-    return executeQuery(true, $sql, [
-      $user->getId(),
+    $result = executeQuery(true, $sql, [
+      $user->getId()
     ]);
+
+    $_SESSION['projects'] = $result;
   }
 
+  getProjectsByUser(); 
